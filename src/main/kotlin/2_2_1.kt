e00347d2c0e8f6e85e69cfb24801490c947ae2e4
VitalKr <vitalkr@mail.ru>
2022-03-27 11:14:45 +0300

    Initial commit

--- src/main/kotlin/2_2_1.kt
+++ src/main/kotlin/2_2_1.kt
@@ -6,6 +6,10 @@ fun main(args: Array<String>) {
     WallService.add(Post(1, 230322, "Заметка4"))
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 3baccfa (Initial commit)
     WallService.update(Post(3, 30, "0000000"))
 }
 
