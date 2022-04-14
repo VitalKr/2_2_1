0a72ea42800d06c2e526d6d0c9afd189ed6cbf76
VitalKr <vitalkr@mail.ru>
2022-04-14 15:02:41 +0300

    Commit 3_1_1

--- src/main/kotlin/2_2_1.kt
+++ src/main/kotlin/2_2_1.kt
@@ -1,11 +1,11 @@
 fun main(args: Array<String>) {
 
-    WallService.add(Post(1, 200322, "Заметка1", null, null,null,null))
-    WallService.add(Post(1, 210322, "Заметка2", null, null,null,null))
-    WallService.add(Post(1, 220322, "Заметка3", null, null,null,null))
-    WallService.add(Post(1, 230322, "Заметка4",null, null,null,null))
+    WallService.add(Post(1, 1,200322, "Заметка1", null, null,null,null))
+    WallService.add(Post(1, 2,210322, "Заметка2", null, null,null,null))
+    WallService.add(Post(1, 3,220322, "Заметка3", null, null,null,null))
+    WallService.add(Post(1, 4,230322, "Заметка4",null, null,null,null))
 
-    WallService.update(Post(3, 30, "0000000",null, null,null,null))
+    WallService.update(Post(3, 2,30, "0000000",null, null,null,null))
 }
 
 
